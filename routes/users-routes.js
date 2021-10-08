ed5fcc0780d3bd57878e1f9eca4535a2478eb76c
David Reusch <dbreusch@me.com>
2021-10-08 10:39:40 -0700

    Restore image file upload logic

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 // initialize router
 const router = express.Router();
@@ -13,6 +14,7 @@ router.get('/', usersControllers.getUsers);
 // /register: handle user signup requests
 router.post(
   '/register',
+  fileUpload.single('image'),
   [
     check('name')
       .not()
@@ -31,14 +33,14 @@ router.post(
   '/login',
   // Validation will normally be handled by frontend but still having it here is
   // useful for other clients.
-  [
-    check('email')
-      .isEmail()
-      .normalizeEmail(),
-    check('password')
-      .not()
-      .isEmpty()
-  ],
+  // [
+  //   check('email')
+  //     .isEmail()
+  //     .normalizeEmail(),
+  //   check('password')
+  //     .not()
+  //     .isEmpty()
+  // ],
   usersControllers.loginUser
 );
 
